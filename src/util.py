a8a4c49981b1f49f711873e71b4e6825ba27e5aa
Akhil Pandey <akhilpandey.akella@gmail.com>
2020-06-16 17:15:02 +0000

    Issue #13: Implemented Bayesian on SmilesLSTM arch (#18)
    - updated the SmilesLSTM class object with TF2 like custom model
    structure and training steps using gradient tape
    - implemented bayesian recurrent neural network for the SmilesLSTM
    architecture
    - implemented plotting function for the Bayesian network in util,py
    
    Signed-off-by: akhilpandey95 <akhilpandey.akella@gmail.com>

---
+++ src/util.py
@@ -0,0 +1,85 @@
+# This Source Code Form is subject to the terms of the MIT
+# License. If a copy of the same was not distributed with this
+# file, You can obtain one at
+# https://github.com/akhilpandey95/uncertainty/blob/master/LICENSE.
+
+# Dependency imports
+from absl import app
+from absl import flags
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import figure  # pylint: disable=g-import-not-at-top
+from matplotlib.backends import backend_agg
+import numpy as np
+import tensorflow.compat.v2 as tf
+import tensorflow_probability as tfp
+
+def plot_weight_posteriors(names, qm_vals, qs_vals, fname):
+  """Save a PNG plot with histograms of weight means and stddevs.
+
+  Args:
+    names: A Python `iterable` of `str` variable names.
+      qm_vals: A Python `iterable`, the same length as `names`,
+      whose elements are Numpy `array`s, of any shape, containing
+      posterior means of weight varibles.
+    qs_vals: A Python `iterable`, the same length as `names`,
+      whose elements are Numpy `array`s, of any shape, containing
+      posterior standard deviations of weight varibles.
+    fname: Python `str` filename to save the plot to.
+  """
+  fig = figure.Figure(figsize=(6, 3))
+  canvas = backend_agg.FigureCanvasAgg(fig)
+
+  ax = fig.add_subplot(1, 2, 1)
+  for n, qm in zip(names, qm_vals):
+    sns.distplot(tf.reshape(qm, shape=[-1]), ax=ax, label=n)
+  ax.set_title('weight means')
+  ax.set_xlim([-1.5, 1.5])
+  ax.legend()
+
+  ax = fig.add_subplot(1, 2, 2)
+  for n, qs in zip(names, qs_vals):
+    sns.distplot(tf.reshape(qs, shape=[-1]), ax=ax)
+  ax.set_title('weight stddevs')
+  ax.set_xlim([0, 1.])
+
+  fig.tight_layout()
+  canvas.print_figure(fname, format='png')
+  print('saved {}'.format(fname))
+
+
+def plot_heldout_prediction(input_vals, probs,
+                            fname, n=10, title=''):
+  """Save a PNG plot visualizing posterior uncertainty on heldout data.
+
+  Args:
+    input_vals: A `float`-like Numpy `array` of shape
+      `[num_heldout] + IMAGE_SHAPE`, containing heldout input images.
+    probs: A `float`-like Numpy array of shape `[num_monte_carlo,
+      num_heldout, num_classes]` containing Monte Carlo samples of
+      class probabilities for each heldout sample.
+    fname: Python `str` filename to save the plot to.
+    n: Python `int` number of datapoints to vizualize.
+    title: Python `str` title for the plot.
+  """
+  fig = figure.Figure(figsize=(9, 3*n))
+  canvas = backend_agg.FigureCanvasAgg(fig)
+  for i in range(n):
+    ax = fig.add_subplot(n, 3, 3*i + 1)
+    ax.imshow(input_vals[i, :].reshape(IMAGE_SHAPE[:-1]), interpolation='None')
+
+    ax = fig.add_subplot(n, 3, 3*i + 2)
+    for prob_sample in probs:
+      sns.barplot(np.arange(10), prob_sample[i, :], alpha=0.1, ax=ax)
+      ax.set_ylim([0, 1])
+    ax.set_title('posterior samples')
+
+    ax = fig.add_subplot(n, 3, 3*i + 3)
+    sns.barplot(np.arange(10), tf.reduce_mean(probs[:, i, :], axis=0), ax=ax)
+    ax.set_ylim([0, 1])
+    ax.set_title('predictive probs')
+  fig.suptitle(title)
+  fig.tight_layout()
+
+  canvas.print_figure(fname, format='png')
+  print('saved {}'.format(fname))

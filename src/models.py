dd5edce93ab0fc1800939e4f537f56d56f3d2566
Akhil Pandey <akhilpandey.akella@gmail.com>
2020-07-24 10:55:52 -0500

    Issue #26: Added custom Gaussian layer for tf2 (#38)
    - removed the old class object definition of gaussian layer from
    src/models.py
    - added the adjusted GaussianLayer class definition to src/layers.py
    with automatic input shape adjustments
    - added the gaussian loss function class object to src/util.py and
    removed old methods
    
    Signed-off-by: akhilpandey95 <akhilpandey.akella@gmail.com>

--- src/models.py
+++ src/models.py
@@ -489,57 +489,6 @@ class GCNConv(tf.keras.layers.Layer):
 
         return output
 
-class GaussianLayer(tf.keras.layers.Layer):
-    """
-    Class object for gaussian layer
-
-    Parameters
-    ----------
-    No arguments
-
-    Returns
-    -------
-    Neural Network Layers
-        tf.keras.layers.Layer
-
-    """
-    # init the gaussian
-    def __init__(self, output_dim, **kwargs):
-        self.output_dim = output_dim
-        super(GaussianLayer, self).__init__(**kwargs)
-
-    # build the tf layer
-    def build(self, input_shape):
-        self.kernel_1 = self.add_weight(name='kernel_1',
-                                      shape=(30, self.output_dim),
-                                      initializer=glorot_normal(),
-                                      trainable=True)
-        self.kernel_2 = self.add_weight(name='kernel_2',
-                                      shape=(30, self.output_dim),
-                                      initializer=glorot_normal(),
-                                      trainable=True)
-        self.bias_1 = self.add_weight(name='bias_1',
-                                    shape=(self.output_dim, ),
-                                    initializer=glorot_normal(),
-                                    trainable=True)
-        self.bias_2 = self.add_weight(name='bias_2',
-                                    shape=(self.output_dim, ),
-                                    initializer=glorot_normal(),
-                                    trainable=True)
-        super(GaussianLayer, self).build(input_shape)
-
-    # tf function to call the layer
-    def call(self, x):
-        output_mu  = K.dot(x, self.kernel_1) + self.bias_1
-        output_sig = K.dot(x, self.kernel_2) + self.bias_2
-        output_sig_pos = K.log(1 + K.exp(output_sig)) + 1e-06
-
-        # return the input
-        return [output_mu, output_sig_pos]
-
-    # tf function to compute the sh
-    def compute_output_shape(self, input_shape):
-        return [(input_shape[0], self.output_dim), (input_shape[0], self.output_dim)]
 
 # Graph neural network (GNN) for learning graph representations
 class GCN(Base):
--- src/util.py
+++ src/util.py
@@ -3,113 +3,27 @@
 # file, You can obtain one at
 # https://github.com/akhilpandey95/uncertainty/blob/master/LICENSE.
 
-# Dependency imports
-from absl import app
-from absl import flags
-import matplotlib
-matplotlib.use('Agg')
-from matplotlib import figure  # pylint: disable=g-import-not-at-top
-from matplotlib.backends import backend_agg
 import numpy as np
-import tensorflow.compat.v2 as tf
-import tensorflow_probability as tfp
-
-def plot_weight_posteriors(names, qm_vals, qs_vals, fname):
-  """Save a PNG plot with histograms of weight means and stddevs.
-
-  Args:
-    names: A Python `iterable` of `str` variable names.
-      qm_vals: A Python `iterable`, the same length as `names`,
-      whose elements are Numpy `array`s, of any shape, containing
-      posterior means of weight varibles.
-    qs_vals: A Python `iterable`, the same length as `names`,
-      whose elements are Numpy `array`s, of any shape, containing
-      posterior standard deviations of weight varibles.
-    fname: Python `str` filename to save the plot to.
-  """
-  fig = figure.Figure(figsize=(6, 3))
-  canvas = backend_agg.FigureCanvasAgg(fig)
-
-  ax = fig.add_subplot(1, 2, 1)
-  for n, qm in zip(names, qm_vals):
-    sns.distplot(tf.reshape(qm, shape=[-1]), ax=ax, label=n)
-  ax.set_title('weight means')
-  ax.set_xlim([-1.5, 1.5])
-  ax.legend()
-
-  ax = fig.add_subplot(1, 2, 2)
-  for n, qs in zip(names, qs_vals):
-    sns.distplot(tf.reshape(qs, shape=[-1]), ax=ax)
-  ax.set_title('weight stddevs')
-  ax.set_xlim([0, 1.])
-
-  fig.tight_layout()
-  canvas.print_figure(fname, format='png')
-  print('saved {}'.format(fname))
+import tensorflow as tf
 
 
-def plot_heldout_prediction(input_vals, probs,
-                            fname, n=10, title=''):
-  """Save a PNG plot visualizing posterior uncertainty on heldout data.
+class GaussLoss(tf.keras.losses.Loss):
+    """
+    Class object for gaussian loss
+    Parameters
+    ----------
+    arg1 | sigma: float32
+        Value of sigma
+    Returns
+    -------
+    Neural Network Loss Function
+        tf.keras.losses.Loss
+    """
 
-  Args:
-    input_vals: A `float`-like Numpy `array` of shape
-      `[num_heldout] + IMAGE_SHAPE`, containing heldout input images.
-    probs: A `float`-like Numpy array of shape `[num_monte_carlo,
-      num_heldout, num_classes]` containing Monte Carlo samples of
-      class probabilities for each heldout sample.
-    fname: Python `str` filename to save the plot to.
-    n: Python `int` number of datapoints to vizualize.
-    title: Python `str` title for the plot.
-  """
-  fig = figure.Figure(figsize=(9, 3*n))
-  canvas = backend_agg.FigureCanvasAgg(fig)
-  for i in range(n):
-    ax = fig.add_subplot(n, 3, 3*i + 1)
-    ax.imshow(input_vals[i, :].reshape(IMAGE_SHAPE[:-1]), interpolation='None')
+    def __init__(self, sigma, name='GaussLoss'):
+        super().__init__(name=name)
+        self.sigma = sigma
 
-    ax = fig.add_subplot(n, 3, 3*i + 2)
-    for prob_sample in probs:
-      sns.barplot(np.arange(10), prob_sample[i, :], alpha=0.1, ax=ax)
-      ax.set_ylim([0, 1])
-    ax.set_title('posterior samples')
-
-    ax = fig.add_subplot(n, 3, 3*i + 3)
-    sns.barplot(np.arange(10), tf.reduce_mean(probs[:, i, :], axis=0), ax=ax)
-    ax.set_ylim([0, 1])
-    ax.set_title('predictive probs')
-  fig.suptitle(title)
-  fig.tight_layout()
-
-  canvas.print_figure(fname, format='png')
-  print('saved {}'.format(fname))
-
-# function for plotting training via deep ensembles
-def plot_training_deep_ens():
-    preds, sigmas = [], []
-    for j in range(len(train_x)):
-        mu, sigma = get_intermediate([[train_x[j]]])
-        preds.append(mu.reshape(1,)[0])
-        sigmas.append(sigma.reshape(1,)[0])plt.figure(1, figsize=(15, 9))
-
-    plt.plot([i[0] for i in train_x], [i for i in train_y])
-    plt.plot([i[0] for i in train_x], [i for i in preds], 'b', linewidth=3)
-    upper = [i+k for i,k in zip(preds, sigmas)]
-    lower = [i-k for i,k in zip(preds, sigmas)]plt.plot([i[0] for i in train_x], [i for i in upper], 'r', linewidth = 3)
-    plt.plot([i[0] for i in train_x], [i for i in lower], 'r', linewidth = 3)
-    plt.plot([i[0] for i in train_x], [pow_fun(i[0]) for i in train_x], 'y', linewidth = 2)
-
-# function for plotting testing via deep ensembles
-def plot_test_deep_ens():
-    x_ax = np.linspace(-4, 4, num=200)
-    preds, sigmas = [], []
-    for j in range(len(x_ax)):
-        mu, sigma = get_intermediate([[np.array([x_ax[j]])]])
-        preds.append(mu.reshape(1,)[0])
-        sigmas.append(sigma.reshape(1,)[0])plt.figure(1, figsize=(15, 9))
-
-    plt.plot([i for i in x_ax], [i for i in preds], 'b', linewidth=3)
-    upper = [i+k for i,k in zip(preds, sigmas)]
-    lower = [i-k for i,k in zip(preds, sigmas)]plt.plot([i for i in x_ax], [i for i in upper], 'r', linewidth = 3)
-    plt.plot([i for i in x_ax], [i for i in lower], 'r', linewidth = 3)
-    plt.plot([i for i in x_ax], [pow_fun(i) for i in x_ax], 'y', linewidth = 2)
+    def call(self, y_true, y_pred):
+        return tf.math.reduce_mean(0.5*tf.math.log(self.sigma) +
+                                   0.5*tf.math.divide(tf.math.square(y_true - y_pred), self.sigma)) + 1e-6

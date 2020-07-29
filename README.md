6ae15091fb8e9c745ab844f628f2fab836fe1fbc
akhilpandey95 <akhilpandey.akella@gmail.com>
2020-07-29 14:27:33 -0500

    Issue #39: Added custom implementations of GCN
    - resolved the tensor stacking issue
    - expanded the README with more details about the experiments and the
    results
    - added test_gcn_qm7.py to test the running of the GCN model on qm7
    dataset
    - added negative log likelihood function in util.py
    
    Signed-off-by: akhilpandey95 <akhilpandey.akella@gmail.com>

--- README.md
+++ README.md
@@ -16,17 +16,20 @@ Install the appropriate python packages
 pip install -r requirements.txt
 ```
 
-## Experiments
-We generated a `sin` dataset of sample size 1201 with gaussian noise given by, `y = 10 sin(x) + ε`. We built simple deep neural networks for predicting the response variable. We then estimated the uncertainties in the models predicting the response values. We tried different approaches and as a result some of the methods that we used supported quantification of both aleatoric, and epistemic uncertainties, while the remaining weren't equipped for the same.
-
-The dataset we used looks like this: 
+## Data
+We have used two datasets. The idea was to perform uncertainty quantification on Fully Connected Networks, and Graph Neural Networks. So, for the Fully Connected Networks we generated a `sin` dataset of sample size 1201 with gaussian noise given by, `y = 10 sin(x) + ε`. The dataset looks like this:
 
 <img src="src/media/data.png" alt="data" style="display: block; margin-right: auto"/>
 
-The approaches we have tried could potentially be categorized into ones that are capable of estimating both aleatoric, and epistemic uncertainties, and others that can only estimate epistemic uncertainties.
+For the Graph Neural Networks, we have used the Quantum Chemistry dataset, `QM7`.
+
+Both of the datasets have single output and would be mapped as regression problems.
+
+## Experiments
+We built simple deep neural networks for predicting the response variable. We then estimated the uncertainties in the models predicting the response values. We tried different approaches and as a result some of the methods that we used supported quantification of both aleatoric, and epistemic uncertainties, while the remaining weren't equipped for the same. The approaches we have tried could potentially be categorized into ones that are capable of estimating both aleatoric, and epistemic uncertainties, and others that can only estimate epistemic uncertainties.
 
 *Methods that can approximate both aleatoric and epistemic uncertainty :*
-- **Bayesian Neural Network** : [Bayesian Learning for Neural Networks by R.M. Neal](http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.446.9306&rep=rep1&type=pdf)
+- **Bayesian Neural Network (BNN)** : [Bayesian Learning for Neural Networks by R.M. Neal](http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.446.9306&rep=rep1&type=pdf)
 - **SQR+ONC** : [Single-Model Uncertainties for Deep Learning by N.Tagasovska, and D. Lopez-Paz](https://arxiv.org/pdf/1811.00908.pdf)
 <hr>
 
@@ -35,6 +38,55 @@ The approaches we have tried could potentially be categorized into ones that are
 - **MC Dropout** : [Dropout as a Bayesian Approximation: Representing Model Uncertainty in Deep Learning by Y. Gal, and Z. Ghahramani](https://arxiv.org/pdf/1506.02142.pdf)
 - **Ensemble MC Dropout** : [Understanding Measures of Uncertainty for Adversarial Example Detection by L. Smith, and Y. Gal](https://arxiv.org/pdf/1803.08533.pdf)
 
+
+For the models predicting the y values of the `sin` dataset, we have used a simple one hidden layer fully connected neural network. The following are the architectural details:
+
+| Model  | Optimum hyper parameters |
+|--------|:------------------------:|
+| **Epochs** | 200, <br> 1500 for BNN |
+| **Batch size** | 128 |
+| **Loss Function** | MSE, <br> Gaussian Loss for DeepEnsemble, <br> Negative log likelihood for BNN, <br> Pinball Loss for SQR+ONC |
+| **Hidden Layers** | 1 layer with 256 neurons |
+| **Optimization function** | Adam with 0.001 learning rate |
+| **Activation function(s)** | ReLU for the hidden layer, <br>Linear for the o/p layer |
+
+
+## Results
+
+Linear regression on the `sin` dataset: 
+
+| Model  | MSE | MAE| RMSE | R2 |
+|--------|:--------------:|:------------:|:---------:|:------:|
+| **Fully Connected Network (FCN)** | 1.0053 | 0.8078 | 1.0026 | 0.9813 |
+| **FCN with MC Dropout** | 1.4041 | 0.9555 | 1.1849 | 0.9740 |
+| **FCN with Ensemble MC Dropout** | 1.4430 | 0.9697 | 1.2012 | 0.9733 |
+| **FCN with DeepEnsembles** | 0.9531 | 0.7767 | 0.9763 | 0.9820 |
+| **FCN with SQR, and ONC** | 1.0929 | 0.8318 | 1.0454 | 0.9793 |
+| **Bayesian Neural Network** | 3.3879 | 1.4489 | 1.9418 | 0.953 |
+
+Linear regression on the `QM7` dataset :
+
+On the test set,
+
+| Model  | MSE | MAE| RMSE | R2 |
+|--------|:--------------:|:------------:|:---------:|:------:|
+| **Graph Convolutional Network (GCN)** | - | - | - | - |
+| **GCN with MC Dropout** | - | - | - | - |
+| **GCN with Ensemble MC Dropout** | - | - | - | - |
+| **GCN with DeepEnsembles** | - | - | - | - |
+| **GCN with SQR, and ONC** | - | - | - | - |
+| **Bayesian Graph Neural Network** | - | - | - | - |
+
+On the validation set,
+
+| Model  | MSE | MAE| RMSE | R2 |
+|--------|:--------------:|:------------:|:---------:|:------:|
+| **Graph Convolutional Network (GCN)** | - | - | - | - |
+| **GCN with MC Dropout** | - | - | - | - |
+| **GCN with Ensemble MC Dropout** | - | - | - | - |
+| **GCN with DeepEnsembles** | - | - | - | - |
+| **GCN with SQR, and ONC** | - | - | - | - |
+| **Bayesian Graph Neural Network** | - | - | - | - |
 
 ## Author
 [Akhil Pandey](https://github.com/akhilpandey95)
--- src/test_gcn_qm7.py
+++ src/test_gcn_qm7.py
@@ -6,9 +6,11 @@
 import numpy as np
 import deepchem as dc
 import tensorflow as tf
+import tensorflow.keras.backend as K
 from tensorflow.keras import layers
 from deepchem.feat.mol_graphs import ConvMol
-from layers import GraphConv, GraphPool, GraphGather
+from layers import GraphConv, GraphPool, GraphGather, TrimGraphOutput
+# from deepchem.models.layers import GraphConv, GraphPool, GraphGather, TrimGraphOutput
 
 # load the qm7 dataset
 tasks, datasets, transformers = dc.molnet.load_qm7_from_mat(
@@ -17,28 +19,91 @@ train_dataset, valid_dataset, test_dataset = datasets
 
 # set the hyperparameters and metrics for the model
 batch_size = 64
-num_epochs = 5
+num_epochs = 50
 n_tasks = 1
 metric = [
     dc.metrics.Metric(dc.metrics.mean_absolute_error, mode="regression"),
-    dc.metrics.Metric(dc.metrics.pearson_r2_score, mode="regression")
+    dc.metrics.Metric(dc.metrics.mean_squared_error, mode="regression"),
+    dc.metrics.Metric(dc.metrics.r2_score, mode="regression")
 ]
 
+def _make_shapes_consistent(output, labels):
+    """Try to make inputs have the same shape by adding dimensions of size 1."""
+    shape1 = output.shape
+    shape2 = labels.shape
+    len1 = len(shape1)
+    len2 = len(shape2)
+    if len1 == len2:
+        return (output, labels)
+    if isinstance(shape1, tf.TensorShape):
+        shape1 = tuple(shape1.as_list())
+    if isinstance(shape2, tf.TensorShape):
+        shape2 = tuple(shape2.as_list())
+    if len1 > len2 and all(i == 1 for i in shape1[len2:]):
+        for i in range(len1 - len2):
+            labels = tf.expand_dims(labels, -1)
+        return (output, labels)
+    if len2 > len1 and all(i == 1 for i in shape2[len1:]):
+        for i in range(len2 - len1):
+            output = tf.expand_dims(output, -1)
+        return (output, labels)
+    raise ValueError("Incompatible shapes for outputs and labels: %s versus %s" %
+                   (str(shape1), str(shape2)))
 
-def data_generator(dataset, epochs=1, predict=False, pad_batches=True):
+def reshape_y_pred(y_true, y_pred):
+    """
+    Function for reshaping the predictions
+    to eliminate negative probability outputs
+    Parameters
+    ----------
+    arg1 | y_true: tf.float32
+        True value of y
+    arg2 | y_pred: tf.float32
+        Predicted value of y
+    Returns
+    -------
+    Array
+        list
+    """
+    n_samples = len(y_true)
+    return y_pred[:n_samples, :]
+
+class QuantileLoss(dc.models.losses.Loss):
+    """
+    Custom Pinball loss function for training GCN
+    Parameters
+    ----------
+    arg1 | tau: int
+        Quantile value
+    Returns
+    -------
+    Float
+        tf.float32
+    """
+    def __init__(self, tau):
+        super().__init__()
+        self.tau = tau
+
+    def __call__(self, output, labels):
+        output, labels = _make_shapes_consistent(output, labels)
+        diff = tf.math.subtract(output, labels)
+        return K.mean(K.maximum(self.tau * diff, (self.tau - 1) * diff), axis=-1)
+
+def data_generator(dataset, batch_size=None, epochs=1, predict=False, pad_batches=True):
     """
     Data generator for training the deepchem GCN model
     a single hidden layer
     Parameters
     ----------
-    No arguments
     arg1 | dataset: dc.data.DiskDataset
         Input dataset to the generator
-    arg2 | epochs: int
+    arg1 | batch_size: int
+        Size of every mini batch
+    arg3 | epochs: int
         Number of iterations/epochs
-    arg3 | predict: bool
+    arg4 | predict: bool
         Boolean for predicting on the inputs of the dataset
-    arg4 | pad_batches: bool
+    arg5 | pad_batches: bool
         Boolean for padding the batches
     Returns
     -------
@@ -46,11 +111,14 @@ def data_generator(dataset, epochs=1, predict=False, pad_batches=True):
         (numpy.ndarray32, numpy.ndarray32, numpy.ndarray32)
     """
     for epoch in range(epochs):
-        for ind, (X_b, y_b, w_b, ids_b) in enumerate(dataset.iterbatches(batch_size,
+        for ind, (X_b, y_b, w_b, ids_b) in enumerate(dataset.iterbatches(64,
                                                                          pad_batches=pad_batches,
                                                                          deterministic=True)):
             # init the ConvMol obj to get the inputs
             multiConvMol = ConvMol.agglomerate_mols(X_b)
+
+            # get the number of samples
+            n_samples = np.array(X_b.shape[0])
 
             # prepare the inputs
             inputs = [multiConvMol.get_atom_features(),
@@ -66,7 +134,6 @@ def data_generator(dataset, epochs=1, predict=False, pad_batches=True):
 
             # yield on  X, y, w
             yield (inputs, outputs, weights)
-
 
 def create_gcn_model():
     """
@@ -84,6 +151,7 @@ def create_gcn_model():
     atom_features = layers.Input(shape=(75,))
     degree_slice = layers.Input(shape=(2,), dtype=tf.int32)
     membership = layers.Input(shape=tuple(), dtype=tf.int32)
+    n_samples = layers.Input(shape=tuple(), dtype=tf.int32)
 
     deg_adjs = []
     for i in range(0, 10 + 1):
@@ -123,11 +191,15 @@ def create_gcn_model():
 
     # output layer
     out = layers.Dense(1, name='output')(readout)
+    # out = TrimGraphOutput()([out, n_samples])
 
+    # inputs = [atom_features, degree_slice, membership, n_samples] + deg_adjs
     inputs = [atom_features, degree_slice, membership] + deg_adjs
+    outputs = [out]
+    # outputs = [out, readout]
 
     # create the model
-    model = tf.keras.Model(inputs=[inputs], outputs=[out])
+    model = tf.keras.Model(inputs=[inputs], outputs=[outputs])
 
     # return the model
     return model
@@ -137,7 +209,10 @@ def create_gcn_model():
 gcn_model = create_gcn_model()
 
 # setup the loss function
-loss = dc.models.losses.L1Loss()
+loss = dc.models.losses.L2Loss()
+# loss = QuantileLoss(0.025)
+# loss = QuantileLoss(0.5)
+# loss = QuantileLoss(0.975)
 
 # build the keras model of deepchem
 model = dc.models.KerasModel(gcn_model, loss=loss)
@@ -151,3 +226,12 @@ for i in range(num_epochs):
     loss = model.fit_generator(data_generator(train_dataset, epochs=1))
     print("Epoch %d loss: %f" % (i, loss))
     losses.append(loss)
+
+
+print("Evaluating model")
+preds = model.predict_on_generator(data_generator(test_dataset, predict=True))
+preds = reshape_y_pred(test_dataset.y, preds)
+mae_scores = metric[0].compute_metric(test_dataset.y, preds, test_dataset.w)
+# mse_scores = metric[1].compute_metric(test_dataset.y, preds, test_dataset.w)
+# r2_scores = metric[2].compute_metric(test_dataset.y, preds, test_dataset.w)
+# print("Test MAE Score: %f" % mae_scores)

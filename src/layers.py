c92aae701489915f2d8affa54a33b66892f7dbd1
Akhil Pandey <akhilpandey.akella@gmail.com>
2020-07-29 14:30:19 -0500

    Issue #39: Added custom implementations of GCN (#41)
    - resolved the tensor stacking issue
    - expanded the README with more details about the experiments and the
    results
    - added test_gcn_qm7.py to test the running of the GCN model on qm7
    dataset
    - added negative log likelihood function in util.py
    
    Signed-off-by: akhilpandey95 <akhilpandey.akella@gmail.com>

--- src/layers.py
+++ src/layers.py
@@ -61,278 +61,184 @@ class GaussianLayer(tf.keras.layers.Layer):
         return [(input_shape[0], self.output_dim), (input_shape[0], self.output_dim)]
 
 
-# class definition for Graph convolution layer
+class TrimGraphOutput(tf.keras.layers.Layer):
+
+  def __init__(self, **kwargs):
+    super(TrimGraphOutput, self).__init__(**kwargs)
+
+  def call(self, inputs):
+    n_samples = tf.squeeze(inputs[1])
+    return inputs[0][0:n_samples]
+
 class GraphConv(tf.keras.layers.Layer):
-    """Graph Convolutional Layers
 
-    This layer implements the graph convolution introduced in
+  def __init__(self,
+               out_channel,
+               min_deg=0,
+               max_deg=10,
+               activation_fn=None,
+               **kwargs):
+    super(GraphConv, self).__init__(**kwargs)
+    self.out_channel = out_channel
+    self.min_degree = min_deg
+    self.max_degree = max_deg
+    self.activation_fn = activation_fn
 
-    Duvenaud, David K., et al. "Convolutional networks on graphs for learning molecular fingerprints." Advances in neural information processing systems. 2015. https://arxiv.org/abs/1509.09292
+  def build(self, input_shape):
+    # Generate the nb_affine weights and biases
+    num_deg = 2 * self.max_degree + (1 - self.min_degree)
+    self.W_list = [
+        self.add_weight(
+            name='kernel',
+            shape=(int(input_shape[0][-1]), self.out_channel),
+            initializer='glorot_uniform',
+            trainable=True) for k in range(num_deg)
+    ]
+    self.b_list = [
+        self.add_weight(
+            name='bias',
+            shape=(self.out_channel,),
+            initializer='zeros',
+            trainable=True) for k in range(num_deg)
+    ]
+    self.built = True
 
-    The graph convolution combines per-node feature vectures in a
-    nonlinear fashion with the feature vectors for neighboring nodes.
-    This "blends" information in local neighborhoods of a graph.
-    """
+  def call(self, inputs):
 
-    def __init__(self,
-                 out_channel,
-                 min_deg=0,
-                 max_deg=10,
-                 activation_fn=None,
-                 **kwargs):
-        """Initialize a graph convolutional layer.
+    # Extract atom_features
+    atom_features = inputs[0]
 
-        Parameters
-        ----------
-        out_channel: int
-          The number of output channels per graph node.
-        min_deg: int, optional (default 0)
-          The minimum allowed degree for each graph node.
-        max_deg: int, optional (default 10)
-          The maximum allowed degree for each graph node. Note that this
-          is set to 10 to handle complex molecules (some organometallic
-          compounds have strange structures). If you're using this for
-          non-molecular applications, you may need to set this much higher
-          depending on your dataset.
-        activation_fn: function
-          A nonlinear activation function to apply. If you're not sure,
-          `tf.nn.relu` is probably a good default for your application.
-        """
-        super(GraphConv, self).__init__(**kwargs)
-        self.out_channel = out_channel
-        self.min_degree = min_deg
-        self.max_degree = max_deg
-        self.activation_fn = activation_fn
+    # Extract graph topology
+    deg_slice = inputs[1]
+    deg_adj_lists = inputs[3:]
 
-    def build(self, input_shape):
-        # Generate the nb_affine weights and biases
-        num_deg = 2 * self.max_degree + (1 - self.min_degree)
-        self.W_list = [
-            self.add_weight(
-                name='kernel',
-                shape=(int(input_shape[0][-1]), self.out_channel),
-                initializer='glorot_uniform',
-                trainable=True) for k in range(num_deg)
-        ]
-        self.b_list = [
-            self.add_weight(
-                name='bias',
-                shape=(self.out_channel,),
-                initializer='zeros',
-                trainable=True) for k in range(num_deg)
-        ]
-        self.built = True
+    W = iter(self.W_list)
+    b = iter(self.b_list)
 
-    def get_config(self):
-        config = super(GraphConv, self).get_config()
-        config['out_channel'] = self.out_channel
-        config['min_deg'] = self.min_degree
-        config['max_deg'] = self.max_degree
-        config['activation_fn'] = self.activation_fn
-        return config
+    # Sum all neighbors using adjacency matrix
+    deg_summed = self.sum_neigh(atom_features, deg_adj_lists)
 
-    def call(self, inputs):
+    # Get collection of modified atom features
+    new_rel_atoms_collection = (self.max_degree + 1 - self.min_degree) * [None]
 
-        # Extract atom_features
-        atom_features = inputs[0]
+    for deg in range(1, self.max_degree + 1):
+      # Obtain relevant atoms for this degree
+      rel_atoms = deg_summed[deg - 1]
 
-        # Extract graph topology
-        deg_slice = inputs[1]
-        deg_adj_lists = inputs[3:]
+      # Get self atoms
+      begin = tf.stack([deg_slice[deg - self.min_degree, 0], 0])
+      size = tf.stack([deg_slice[deg - self.min_degree, 1], -1])
+      self_atoms = tf.slice(atom_features, begin, size)
 
-        W = iter(self.W_list)
-        b = iter(self.b_list)
+      # Apply hidden affine to relevant atoms and append
+      rel_out = tf.matmul(rel_atoms, next(W)) + next(b)
+      self_out = tf.matmul(self_atoms, next(W)) + next(b)
+      out = rel_out + self_out
 
-        # Sum all neighbors using adjacency matrix
-        deg_summed = self.sum_neigh(atom_features, deg_adj_lists)
+      new_rel_atoms_collection[deg - self.min_degree] = out
 
-        # Get collection of modified atom features
-        new_rel_atoms_collection = (
-            self.max_degree + 1 - self.min_degree) * [None]
+    # Determine the min_deg=0 case
+    if self.min_degree == 0:
+      deg = 0
 
-        split_features = tf.split(atom_features, deg_slice[:, 1])
-        for deg in range(1, self.max_degree + 1):
-            # Obtain relevant atoms for this degree
-            rel_atoms = deg_summed[deg - 1]
+      begin = tf.stack([deg_slice[deg - self.min_degree, 0], 0])
+      size = tf.stack([deg_slice[deg - self.min_degree, 1], -1])
+      self_atoms = tf.slice(atom_features, begin, size)
 
-            # Get self atoms
-            self_atoms = split_features[deg - self.min_degree]
+      # Only use the self layer
+      out = tf.matmul(self_atoms, next(W)) + next(b)
 
-            # Apply hidden affine to relevant atoms and append
-            rel_out = tf.matmul(rel_atoms, next(W)) + next(b)
-            self_out = tf.matmul(self_atoms, next(W)) + next(b)
-            out = rel_out + self_out
+      new_rel_atoms_collection[deg - self.min_degree] = out
 
-            new_rel_atoms_collection[deg - self.min_degree] = out
+    # Combine all atoms back into the list
+    atom_features = tf.concat(axis=0, values=new_rel_atoms_collection)
 
-        # Determine the min_deg=0 case
-        if self.min_degree == 0:
-            self_atoms = split_features[0]
+    if self.activation_fn is not None:
+      atom_features = self.activation_fn(atom_features)
 
-            # Only use the self layer
-            out = tf.matmul(self_atoms, next(W)) + next(b)
+    return atom_features
 
-            new_rel_atoms_collection[0] = out
+  def sum_neigh(self, atoms, deg_adj_lists):
+    """Store the summed atoms by degree"""
+    deg_summed = self.max_degree * [None]
 
-        # Combine all atoms back into the list
-        atom_features = tf.concat(axis=0, values=new_rel_atoms_collection)
+    # Tensorflow correctly processes empty lists when using concat
+    for deg in range(1, self.max_degree + 1):
+      gathered_atoms = tf.gather(atoms, deg_adj_lists[deg - 1])
+      # Sum along neighbors as well as self, and store
+      summed_atoms = tf.reduce_sum(gathered_atoms, 1)
+      deg_summed[deg - 1] = summed_atoms
 
-        if self.activation_fn is not None:
-            atom_features = self.activation_fn(atom_features)
-
-        return atom_features
-
-    def sum_neigh(self, atoms, deg_adj_lists):
-        """Store the summed atoms by degree"""
-        deg_summed = self.max_degree * [None]
-
-        # Tensorflow correctly processes empty lists when using concat
-        for deg in range(1, self.max_degree + 1):
-            gathered_atoms = tf.gather(atoms, deg_adj_lists[deg - 1])
-            # Sum along neighbors as well as self, and store
-            summed_atoms = tf.reduce_sum(gathered_atoms, 1)
-            deg_summed[deg - 1] = summed_atoms
-
-        return deg_summed
+    return deg_summed
 
 
 class GraphPool(tf.keras.layers.Layer):
-    """A GraphPool gathers data from local neighborhoods of a graph.
 
-    This layer does a max-pooling over the feature vectors of atoms in a
-    neighborhood. You can think of this layer as analogous to a max-pooling layer
-    for 2D convolutions but which operates on graphs instead.
-    """
+  def __init__(self, min_degree=0, max_degree=10, **kwargs):
+    super(GraphPool, self).__init__(**kwargs)
+    self.min_degree = min_degree
+    self.max_degree = max_degree
 
-    def __init__(self, min_degree=0, max_degree=10, **kwargs):
-        """Initialize this layer
+  def call(self, inputs):
+    atom_features = inputs[0]
+    deg_slice = inputs[1]
+    deg_adj_lists = inputs[3:]
 
-        Parameters
-        ----------
-        min_deg: int, optional (default 0)
-          The minimum allowed degree for each graph node.
-        max_deg: int, optional (default 10)
-          The maximum allowed degree for each graph node. Note that this
-          is set to 10 to handle complex molecules (some organometallic
-          compounds have strange structures). If you're using this for
-          non-molecular applications, you may need to set this much higher
-          depending on your dataset.
-        """
-        super(GraphPool, self).__init__(**kwargs)
-        self.min_degree = min_degree
-        self.max_degree = max_degree
+    # Perform the mol gather
+    # atom_features = graph_pool(atom_features, deg_adj_lists, deg_slice,
+    #                            self.max_degree, self.min_degree)
 
-    def get_config(self):
-        config = super(GraphPool, self).get_config()
-        config['min_degree'] = self.min_degree
-        config['max_degree'] = self.max_degree
-        return config
+    deg_maxed = (self.max_degree + 1 - self.min_degree) * [None]
 
-    def call(self, inputs):
-        atom_features = inputs[0]
-        deg_slice = inputs[1]
-        deg_adj_lists = inputs[3:]
+    # Tensorflow correctly processes empty lists when using concat
 
-        # Perform the mol gather
-        # atom_features = graph_pool(atom_features, deg_adj_lists, deg_slice,
-        #                            self.max_degree, self.min_degree)
+    for deg in range(1, self.max_degree + 1):
+      # Get self atoms
+      begin = tf.stack([deg_slice[deg - self.min_degree, 0], 0])
+      size = tf.stack([deg_slice[deg - self.min_degree, 1], -1])
+      self_atoms = tf.slice(atom_features, begin, size)
 
-        deg_maxed = (self.max_degree + 1 - self.min_degree) * [None]
+      # Expand dims
+      self_atoms = tf.expand_dims(self_atoms, 1)
 
-        # Tensorflow correctly processes empty lists when using concat
+      # always deg-1 for deg_adj_lists
+      gathered_atoms = tf.gather(atom_features, deg_adj_lists[deg - 1])
+      gathered_atoms = tf.concat(axis=1, values=[self_atoms, gathered_atoms])
 
-        split_features = tf.split(atom_features, deg_slice[:, 1])
-        for deg in range(1, self.max_degree + 1):
-            # Get self atoms
-            self_atoms = split_features[deg - self.min_degree]
+      maxed_atoms = tf.reduce_max(gathered_atoms, 1)
+      deg_maxed[deg - self.min_degree] = maxed_atoms
 
-            if deg_adj_lists[deg - 1].shape[0] == 0:
-                # There are no neighbors of this degree, so just create an empty tensor directly.
-                maxed_atoms = tf.zeros((0, self_atoms.shape[-1]))
-            else:
-                # Expand dims
-                self_atoms = tf.expand_dims(self_atoms, 1)
+    if self.min_degree == 0:
+      begin = tf.stack([deg_slice[0, 0], 0])
+      size = tf.stack([deg_slice[0, 1], -1])
+      self_atoms = tf.slice(atom_features, begin, size)
+      deg_maxed[0] = self_atoms
 
-                # always deg-1 for deg_adj_lists
-                gathered_atoms = tf.gather(
-                    atom_features, deg_adj_lists[deg - 1])
-                gathered_atoms = tf.concat(
-                    axis=1, values=[self_atoms, gathered_atoms])
-
-                maxed_atoms = tf.reduce_max(gathered_atoms, 1)
-            deg_maxed[deg - self.min_degree] = maxed_atoms
-
-        if self.min_degree == 0:
-            self_atoms = split_features[0]
-            deg_maxed[0] = self_atoms
-
-        return tf.concat(axis=0, values=deg_maxed)
+    return tf.concat(axis=0, values=deg_maxed)
 
 
 class GraphGather(tf.keras.layers.Layer):
-    """A GraphGather layer pools node-level feature vectors to create a graph feature vector.
 
-    Many graph convolutional networks manipulate feature vectors per
-    graph-node. For a molecule for example, each node might represent an
-    atom, and the network would manipulate atomic feature vectors that
-    summarize the local chemistry of the atom. However, at the end of
-    the application, we will likely want to work with a molecule level
-    feature representation. The `GraphGather` layer creates a graph level
-    feature vector by combining all the node-level feature vectors.
+  def __init__(self, batch_size, activation_fn=None, **kwargs):
+    super(GraphGather, self).__init__(**kwargs)
+    self.batch_size = batch_size
+    self.activation_fn = activation_fn
 
-    One subtlety about this layer is that it depends on the
-    `batch_size`. This is done for internal implementation reasons. The
-    `GraphConv`, and `GraphPool` layers pool all nodes from all graphs
-    in a batch that's being processed. The `GraphGather` reassembles
-    these jumbled node feature vectors into per-graph feature vectors.
-    """
+  def call(self, inputs):
+    # x = [atom_features, deg_slice, membership, deg_adj_list placeholders...]
+    atom_features = inputs[0]
 
-    def __init__(self, batch_size, activation_fn=None, **kwargs):
-        """Initialize this layer.
+    # Extract graph topology
+    membership = inputs[2]
 
-        Parameters
-        ---------
-        batch_size: int
-          The batch size for this layer. Note that the layer's behavior
-          changes depending on the batch size.
-        activation_fn: function
-          A nonlinear activation function to apply. If you're not sure,
-          `tf.nn.relu` is probably a good default for your application.
-        """
+    assert self.batch_size > 1, "graph_gather requires batches larger than 1"
 
-        super(GraphGather, self).__init__(**kwargs)
-        self.batch_size = batch_size
-        self.activation_fn = activation_fn
+    sparse_reps = tf.math.unsorted_segment_sum(atom_features, membership,
+                                          self.batch_size)
+    max_reps = tf.math.unsorted_segment_max(atom_features, membership,
+                                       self.batch_size)
+    mol_features = tf.concat(axis=1, values=[sparse_reps, max_reps])
 
-    def get_config(self):
-        config = super(GraphGather, self).get_config()
-        config['batch_size'] = self.batch_size
-        config['activation_fn'] = self.activation_fn
-        return config
-
-    def call(self, inputs):
-        """Invoking this layer.
-
-        Parameters
-        ----------
-        inputs: list
-          This list should consist of `inputs = [atom_features, deg_slice,
-          membership, deg_adj_list placeholders...]`. These are all
-          tensors that are created/process by `GraphConv` and `GraphPool`
-        """
-        atom_features = inputs[0]
-
-        # Extract graph topology
-        membership = inputs[2]
-
-        assert self.batch_size > 1, "graph_gather requires batches larger than 1"
-
-        sparse_reps = tf.math.unsorted_segment_sum(atom_features, membership,
-                                                   self.batch_size)
-        max_reps = tf.math.unsorted_segment_max(atom_features, membership,
-                                                self.batch_size)
-        mol_features = tf.concat(axis=1, values=[sparse_reps, max_reps])
-
-        if self.activation_fn is not None:
-            mol_features = self.activation_fn(mol_features)
-        return mol_features
+    if self.activation_fn is not None:
+      mol_features = self.activation_fn(mol_features)
+    return mol_features
--- src/util.py
+++ src/util.py
@@ -6,7 +6,7 @@
 import numpy as np
 import tensorflow as tf
 
-
+# custom gaussian loss function
 class GaussLoss(tf.keras.losses.Loss):
     """
     Class object for gaussian loss
@@ -27,3 +27,23 @@ class GaussLoss(tf.keras.losses.Loss):
     def call(self, y_true, y_pred):
         return tf.math.reduce_mean(0.5*tf.math.log(self.sigma) +
                                    0.5*tf.math.divide(tf.math.square(y_true - y_pred), self.sigma)) + 1e-6
+
+
+def neg_log_likelihood_loss(y_true, y_pred):
+    """
+    Kerass loss Function for calculating
+    negative log likelihood loss
+    Parameters
+    ----------
+    arg1 | y_true: tf.float32
+        True value of y
+    arg2 | y_pred: tf.float32
+        Predicted value of y
+    Returns
+    -------
+    Float
+        tf.keras.losses.Loss
+    """
+    sep = y_pred.shape[1] // 2
+    mu, logvar = y_pred[:, :sep], y_pred[:, sep:]
+    return K.sum(0.5*(logvar+np.log(2*np.pi)+K.square((y_true-mu)/K.exp(0.5*logvar))), axis=-1)
